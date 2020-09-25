df6b9dfac08f25224af18326f94295e42e73b0c5
matt <matthewk.rydalch@gmail.com>
2020-09-25 17:31:01 -0600

    fixed seg faulting.  Not sure why it was happening.

--- roscopter/src/tests/rtk_compassing_test.cpp
+++ roscopter/src/tests/rtk_compassing_test.cpp
@@ -55,6 +55,32 @@ void test_quaternion(Compare_states compare, double expectedHeadingEstimate)
   EXPECT_NEAR(expectedHeadingEstimate, euler[2], tolerance);
 }
 
+void test_all_states(Compare_states compare, double expectedHeadingEstimate)
+{
+  double tolerance = 0.001;
+  EXPECT_NEAR(compare.expectedState.x, compare.state.x, tolerance);
+  EXPECT_NEAR(compare.expectedState.y, compare.state.y, tolerance);
+  EXPECT_NEAR(compare.expectedState.z, compare.state.z, tolerance);
+  EXPECT_NEAR(compare.expectedState.qw, compare.state.qw, tolerance);
+  EXPECT_NEAR(compare.expectedState.qx, compare.state.qx, tolerance); 
+  EXPECT_NEAR(compare.expectedState.qy, compare.state.qy, tolerance);  
+  EXPECT_NEAR(compare.expectedState.qz, compare.state.qz, tolerance);
+  EXPECT_NEAR(compare.expectedState.u, compare.state.u, tolerance);
+  EXPECT_NEAR(compare.expectedState.v, compare.state.v, tolerance);
+  EXPECT_NEAR(compare.expectedState.w, compare.state.w, tolerance);
+  EXPECT_NEAR(compare.expectedState.ba_x, compare.state.ba_x, tolerance);
+  EXPECT_NEAR(compare.expectedState.ba_y, compare.state.ba_y, tolerance);
+  EXPECT_NEAR(compare.expectedState.ba_z, compare.state.ba_z, tolerance);
+  EXPECT_NEAR(compare.expectedState.bg_x, compare.state.bg_x, tolerance);
+  EXPECT_NEAR(compare.expectedState.bg_y, compare.state.bg_y, tolerance);
+  EXPECT_NEAR(compare.expectedState.bg_z, compare.state.bg_z, tolerance);
+  EXPECT_NEAR(compare.expectedState.bb, compare.state.bb, tolerance);
+  EXPECT_NEAR(compare.expectedState.ref, compare.state.ref, tolerance);
+
+  std::array<double,3> euler = quaternion_2_euler(compare.state.qw, compare.state.qx, compare.state.qy, compare.state.qz);
+  EXPECT_NEAR(expectedHeadingEstimate, euler[2], tolerance);
+}
+
 TEST(rtkCompassingUpdate, Test0Change)
 {
   double rtkHeading = 0.0; //radians
@@ -112,7 +138,7 @@ TEST(rtkCompassing, TestAllStates)
 
   Compare_states compare = setup_given_relpos_expect_heading_test(rtkHeading, rtkHeadingAccuracy, expectedHeadingEstimate, initialHeading);
 
-  test_quaternion(compare, expectedHeadingEstimate);
+  test_all_states(compare, expectedHeadingEstimate);
 }
 
 // TEST(rtkCompassingUpdate, TestCovariance)
