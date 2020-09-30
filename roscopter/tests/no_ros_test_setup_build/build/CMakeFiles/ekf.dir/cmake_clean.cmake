007c8744f734b94844a6d86d410763672f8cfe18
matt <matthewk.rydalch@gmail.com>
2020-09-30 12:33:30 -0600

    Added the new tests folder, and fixed the failing test.  The new test folder contains some non ros tests that do not work right now.

---
+++ roscopter/tests/no_ros_test_setup_build/build/CMakeFiles/ekf.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ekf.dir/home/matt/ws_rtkflight/src/roscopter/roscopter/src/ekf/dynamics.cpp.o"
+  "CMakeFiles/ekf.dir/home/matt/ws_rtkflight/src/roscopter/roscopter/src/ekf/ekf.cpp.o"
+  "CMakeFiles/ekf.dir/home/matt/ws_rtkflight/src/roscopter/roscopter/src/ekf/log.cpp.o"
+  "CMakeFiles/ekf.dir/home/matt/ws_rtkflight/src/roscopter/roscopter/src/ekf/meas.cpp.o"
+  "CMakeFiles/ekf.dir/home/matt/ws_rtkflight/src/roscopter/roscopter/src/ekf/sensors.cpp.o"
+  "CMakeFiles/ekf.dir/home/matt/ws_rtkflight/src/roscopter/roscopter/src/ekf/state.cpp.o"
+  "libekf.a"
+  "libekf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ekf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
